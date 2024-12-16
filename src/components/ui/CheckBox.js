566e67af7500546e8512e3c89d6f51ea46290a27
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2024-12-16 11:22:16 +0530

    Confirmation on Completing a Task:added

--- src/components/ui/CheckBox.js
+++ src/components/ui/CheckBox.js
@@ -1,22 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class CheckBox extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            checked: this.props.checked
+            checked: this.props.checked,
+            showConfirmation: false
         };
     }
 
     handleChange(e) {
-        const {checked} = e.target;
+        const { checked } = e.target;
+        console.log('checked: ', checked);
+        this.setState({ showConfirmation: true, checked: false });
+    }
 
-        this.setState({checked});
-        this.props.onChange(checked);
+    confirmChange() {
+        this.props.onChange(true); 
+        this.setState({ showConfirmation: false });
+    }
+
+    cancelChange() {
+        this.setState({ showConfirmation: false, checked: this.props.checked });
     }
 
     render() {
-        return (<input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>);
+        return (
+            <div>
+                <input
+                    type="checkbox"
+                    checked={this.state.checked}
+                    onChange={this.handleChange.bind(this)}
+                />
+                {this.state.showConfirmation && (
+                    <div className="confirmation-dialog">
+                        <p>Are you sure you want to mark this task as completed?</p>
+                        <button onClick={() => this.confirmChange()}>Confirm</button>
+                        <button onClick={() => this.cancelChange()}>Cancel</button>
+                    </div>
+                )}
+            </div>
+        );
     }
 }
 
